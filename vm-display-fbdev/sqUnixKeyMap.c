151092f74e640450fcfc0ba51604a821283d4b5e
KenDickey <ken.dickey@whidbey.com>
2020-01-01 08:49:07 -0800

    added obligatory copyright

--- vm-display-fbdev/sqUnixKeyMap.c
+++ vm-display-fbdev/sqUnixKeyMap.c
@@ -1,3 +1,34 @@
+/* sqUnixKeyMap.c -- maps key codes into events for the VM
+ *                   in the absence of a window system
+ *                   using libevdev
+ *
+ * Author: Ken.Dickey@whidbey.com
+ *
+ * Copyright (C) 2019,2020 by Kenneth A Dickey
+ * All Rights Reserved.
+ * 
+ * [Donated to the Squeak/Cuis/Pharo Community]
+ *
+ * This file is part of Unix Squeak.
+ * 
+ *   Permission is hereby granted, free of charge, to any person obtaining a
+ *   copy of this software and associated documentation files (the "Software"),
+ *   to deal in the Software without restriction, including without limitation
+ *   the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ *   and/or sell copies of the Software, and to permit persons to whom the
+ *   Software is furnished to do so, subject to the following conditions:
+ * 
+ *   The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ * 
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ *   DEALINGS IN THE SOFTWARE.
+ */
 
 #include <input.h>   /* /usr/include/linux/input.h */
 #include <keysym.h>  /* /usr/include/X11/keysym.h */
