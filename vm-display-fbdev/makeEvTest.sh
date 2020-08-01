e84c6369f54acfe88b6acc8c9e9e40cbc5716022
KenDickey <Ken.Dickey@Whidbey.COM>
2020-08-01 16:42:32 -0700

    Flags for MUSL

--- vm-display-fbdev/makeEvTest.sh
+++ vm-display-fbdev/makeEvTest.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 export LSB_FIRST=1
 export TARGET_ARCH="-march=armv8-a" 
-export CFLAGS=" -D__ARM_ARCH_ISA_A64 -DARM64 -D__arm__ -D__arm64__ -D__aarch64__"
+# for MUSL:  -DMUSL -D_GNU_SOURCE
+export CFLAGS=" -DMUSL -D_GNU_SOURCE -D__ARM_ARCH_ISA_A64 -DARM64 -D__arm__ -D__arm64__ -D__aarch64__"
 export INCLUDES="-I/home/kend/OpenSmalltalk/oscogvm/src/vm -I/home/kend/OpenSmalltalk/oscogvm/platforms/Cross/vm -I/home/kend/OpenSmalltalk/oscogvm/platforms/unix/vm -I/home/kend/OpenSmalltalk/oscogvm/build.linux64ARMv8/squeak.stack.spur/build "
 gcc -o evtest $TARGET_ARCH $CFLAGS $INCLUDES sqEVTest.c
 echo "[Hopefully] compiled evtest"
